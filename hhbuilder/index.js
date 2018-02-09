55df9b5bc3dc064d8403e76601ad091b060b1d91
sramos9 <ramos.samantha9@gmail.com>
2018-02-09 11:23:49 -0600

    first

--- hhbuilder/index.js
+++ hhbuilder/index.js
@@ -1,1 +1,9 @@
-// your code goes here ...
+//Validate data entry (age is required and > 0, relationship is required)
+
+//Add people to a growing household list
+
+//Remove a previously added person from the list
+
+//Display the household list in the HTML as it is modified
+
+//Serialize the household as JSON upon form submission as a fake trip to the server
