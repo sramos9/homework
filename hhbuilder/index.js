52ca5363366088868a0cf42c757ffa5a1a4fab59
sramos9 <ramos.samantha9@gmail.com>
2018-02-09 12:38:18 -0600

    working through some stuff. vanilla js is crap. how can i get the newly created div to display on the page?

--- hhbuilder/index.js
+++ hhbuilder/index.js
@@ -1,5 +1,34 @@
 //Validate data entry (age is required and > 0, relationship is required)
 
+
+
+const validateEntry = ()=> {
+
+  var target = document.querySelector("builder");
+  var div = document.createElement("div");
+  div.innerHTML = 'some text goes here demo';
+
+  var n, text;
+
+   n = document.getElementsByTagName("age").value;
+   if (n == ""){
+    text = "Error age must be submitted";
+    return false;
+    } else if (isNaN(n) || n <= 0) {
+      text = "Input not Valid, please enter your age.";
+      return false;
+    } else {
+        text = "Input OK";
+    };
+    target.parentNode.insertBefore(div, target.nextSibling).innerHTML = text;
+
+  //document.getElementById("target").innerHTML = text;
+  //div.innerHTML = text;
+
+};
+
+validateEntry();
+
 //Add people to a growing household list
 
 //Remove a previously added person from the list
